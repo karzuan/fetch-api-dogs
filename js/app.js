433b15e9a51d7f7d53a0dd32c4f0ac3f5ff5c9af
Konstantin Kuts <karzuan@gmail.com>
2020-04-10 02:13:46 +0000

    add post data functionality

--- js/app.js
+++ js/app.js
@@ -73,9 +73,28 @@ function putInSelect(breeds){
 
 select.addEventListener('change', fetchBreedImage );
 card.addEventListener('click', fetchBreedImage);
+form.addEventListener('submit', postData );
 
 
 // ------------------------------------------
 //  POST DATA
 // ------------------------------------------
 
+function postData(e) {
+  e.preventDefault();
+  const name = document.getElementById('name').value;
+  const comment = document.getElementById('comment').value;
+  
+  const config = {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ name: name, comment: comment } )
+  };
+  
+  fetch('https://jsonplaceholder.typicode.com/comments', config)
+    .then(checkStatus)
+    .then( res => res.json())
+    .then( data => console.log(data));
+  }
